eb744610967df57f55c6ab561994d09373442e8a
MHanL <38771965+MHanL@users.noreply.github.com>
2019-03-19 12:21:34 +0800

    增加ipa包下载安装，优化代码

--- index.php
+++ index.php
@@ -1,102 +1,159 @@
 <?php
 /**
  * @package Lanzou
- * @author Mlooc
- * @version 1.0.3
+ * @author Filmy
+ * @version 1.1
  * @link https://mlooc.cn
  */
-// 指定允许其他域名访问  
+
 header('Access-Control-Allow-Origin:*');
-	function MloocCurl($url,$method,$ifurl,$post_data){
-		$UserAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36';#设置UserAgent
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);
-		#关闭SSL
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-		#返回数据不直接显示
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-		if ($method == "post") {
-			curl_setopt($curl, CURLOPT_REFERER, $ifurl); 
-			curl_setopt($curl, CURLOPT_POST, 1); 
-        	curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
-		}
-		$response = curl_exec($curl);
-		curl_close($curl);
-		return $response;
-	}
-	if (!empty($_GET['url'])) {
-		$url = $_GET['url'];
-		#判断文件是否被取消
-		if (strstr(MloocCurl($url,null,null,null),"来晚啦...文件取消分享了") != false) {
-			echo "文件取消分享了";
-			exit;
-		}
-		#第一步
-		$ruleMatchDetailInList = "~ifr2\"\sname=\"[\s\S]*?\"\ssrc=\"\/(.*?)\"~";#正则表达式
-		preg_match($ruleMatchDetailInList, MloocCurl($url,null,null,null),$link);
-		$ifurl = "https://www.lanzous.com/".$link[1];
-		#第二步
-		$ruleMatchDetailInList = "~=\s'(.*?)';[\S\s]*?=\s'(.*?)'[\S\s]*?=\s'(.*?)'[\S\s]*?=\s'(.*?)'~";#正则表达式
-		preg_match($ruleMatchDetailInList, MloocCurl($ifurl,null,null,null),$segment);
-		#第三步
-		#post提交的数据
-		$post_data = array(
-			"action" => $segment[1],
-			"file_id" => $segment[2],
-			"t" => $segment[3],
-			"k" => $segment[4]
-			);
-		$obj = json_decode(MloocCurl("https://www.lanzous.com/ajaxm.php","post",$ifurl,$post_data));#json解析
-      	if($obj->inf != "0" && !isset($_GET['pwd'])){
-			echo "该链接需要密码";
-			exit;
-		}elseif ($obj->inf != "0" && isset($_GET['pwd'])) {
-			$post_data = array(
-			"action" => $segment[1],
-			"file_id" => $segment[2],
-			"t" => $segment[3],
-			"k" => $segment[4],
-			"p" => $_GET['pwd']
-			);
-			$obj = json_decode(MloocCurl("https://www.lanzous.com/ajaxm.php","post",$ifurl,$post_data));#json解析
-		}
-		if ($obj->dom == "") {#判断链接是否正确
-			echo "链接有误！";
-			exit;
-		}else{
-			$downUrl = $obj->dom."/file/".$obj->url;
-			if (!empty($_GET['type'])) {
-				$type = $_GET['type'];
-				if ($type == "down") {
-					header('Location:'.$downUrl);#直接下载
-				}else{
-					echo $downUrl;#输出直链
-				}
-			}else{
-				echo $downUrl;#输出直链
-			}
-		}
-	}else{
-		$result_url = str_replace("index.php","","//".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']."?url=https://www.lanzous.com/i1aesgj");
-		$result_url_pwd = str_replace("index.php","","//".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']."?url=https://www.lanzous.com/i19pnjc");
-      	echo "url:蓝奏云外链链接";
-      	echo "<br/><br/>";
-      	echo "type:是否直接下载 值：down";
-      	echo "<br/><br/>";
-      	echo "pwd:外链密码";
-        echo "<br/><br/>";
-		echo "直接下载：";
-		echo "<br/>";
-		echo "无密码：<a href='".$result_url."&type=down' target='_blank'>".$result_url."&type=down</a>";
-		echo "<br/><br/>";
-		echo "有密码：<a href='".$result_url_pwd."&type=down&pwd=1pud' target='_blank'>".$result_url_pwd."&type=down&pwd=1pud</a>";
-		echo "<br/><br/>";
-		echo "输出直链：";
-		echo "<br/>";
-		echo "无密码：<a href='".$result_url."' target='_blank'>".$result_url."</a>";
-		echo "<br/><br/>";
-		echo "有密码：<a href='".$result_url_pwd."&pwd=1pud' target='_blank'>".$result_url_pwd."&pwd=1pud</a>";
-	}
-?>
+header('Content-Type:application/json; charset=utf-8');
+
+$url = isset($_GET['url']) ? $_GET['url'] : "";
+$pwd = isset($_GET['pwd']) ? $_GET['pwd'] : "";
+$type = isset($_GET['type']) ? $_GET['type'] : "";
+if (empty($url)) {
+    die(
+    json_encode(
+        array(
+            'code' => 400,
+            'msg' => '请输入URL'
+        )
+        , JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
+    );
+}
+$softInfo = MloocCurlGet($url);
+if (strstr($softInfo, "文件取消分享了") != false) {
+    die(
+    json_encode(
+        array(
+            'code' => 400,
+            'msg' => '文件取消分享了'
+        )
+        , JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
+    );
+}
+preg_match('~class="b">(.*?)<\/div>~', $softInfo, $softName);
+if (strstr($softInfo, "手机Safari可在线安装") != false) {
+    preg_match('~com/(\w+)~', $url, $lanzouId);
+    if (!isset($lanzouId[1])) {
+        die(
+        json_encode(
+            array(
+                'code' => 400,
+                'msg' => '解析失败，获取不到文件ID'
+            )
+            , JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
+        );
+    }
+    $lanzouId = $lanzouId[1];
+    $ipaInfo = MloocCurlGet("https://www.lanzous.com/tp/" . $lanzouId, 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1');
+    preg_match('~href="(.*?)" id="plist"~', $ipaInfo, $ipaDownUrl);
+    $ipaDownUrl = isset($ipaDownUrl[1]) ? $ipaDownUrl[1] : "";
+    if ($type != "down") {
+        die(
+        json_encode(
+            array(
+                'code' => 200,
+                'msg' => '',
+                'name' => isset($softName[1]) ? $softName[1] : "",
+                'downUrl' => $ipaDownUrl
+            )
+            , JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
+        );
+    } else {
+        header("Location:$ipaDownUrl");
+        die;
+    }
+}
+preg_match('~class="b">(.*?)<\/div>~', $softInfo, $softName);
+preg_match("~ifr2\"\sname=\"[\s\S]*?\"\ssrc=\"\/(.*?)\"~", $softInfo, $link);
+$ifurl = "https://www.lanzous.com/" . $link[1];
+$softInfo = MloocCurlGet($ifurl);
+if (empty($pwd) && strstr($softInfo, "输入密码") != false) {
+    die(
+    json_encode(
+        array(
+            'code' => 400,
+            'msg' => '请输入分享密码'
+        )
+        , JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
+    );
+}
+preg_match("~=\s'(.*?)';[\S\s]*?=\s'(.*?)'[\S\s]*?=\s'(.*?)'[\S\s]*?=\s'(.*?)'~", $softInfo, $segment);
+$post_data = array(
+    "action" => $segment[1],
+    "file_id" => $segment[2],
+    "t" => $segment[3],
+    "k" => $segment[4],
+    "c" => "",
+    "p" => $pwd,
+);
+$softInfo = MloocCurlPost($post_data, "https://www.lanzous.com/ajaxm.php", $ifurl);
+$softInfo = json_decode($softInfo, true);
+if ($softInfo['zt'] != 1) {
+    die(
+    json_encode(
+        array(
+            'code' => 400,
+            'msg' => $softInfo['inf']
+        )
+        , JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
+    );
+}
+$downUrl = $softInfo['dom'] . '/file/' . $softInfo['url'];
+if ($type != "down") {
+    die(
+    json_encode(
+        array(
+            'code' => 200,
+            'msg' => '',
+            'downUrl' => $downUrl
+        )
+        , JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
+    );
+} else {
+    header("Location:$downUrl");
+    die;
+}
+
+function MloocCurlGet($url, $UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36')
+{
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+    if ($UserAgent != "") {
+        curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);
+    }
+
+    #关闭SSL
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    #返回数据不直接显示
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return $response;
+}
+
+function MloocCurlPost($post_data, $url, $ifurl = '', $UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36')
+{
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);
+    if ($ifurl != '') {
+        curl_setopt($curl, CURLOPT_REFERER, $ifurl);
+    }
+
+    #关闭SSL
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    #返回数据不直接显示
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return $response;
+}
