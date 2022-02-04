127acb06768b06f02aa6b40e411a0df3aec46667
寒曦朦 <1017959770@qq.com>
2022-02-04 20:37:11 +0800

    Update index.php
    修改链接处理方案

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ header('Content-Type:application/json; charset=utf-8');
 //默认UA
 $UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36';
 
-//自动替换地址为新版
-$_GET['url']=str_replace('lanzous.com','lanzoux.com',$_GET['url']);
+//一个简单的链接处理
+$_GET['url']='https://www.lanzoux.com/'.explode('.com/',$_GET['url'])['1'];
 $url = isset($_GET['url']) ? $_GET['url'] : "";
 $pwd = isset($_GET['pwd']) ? $_GET['pwd'] : "";
 $type = isset($_GET['type']) ? $_GET['type'] : "";
